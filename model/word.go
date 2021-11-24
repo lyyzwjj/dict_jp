78d84aca8d55eedc62b04016ec8788167b0fcf37
wjj <wzzst310@163.com>
2021-11-24 19:16:07 +0800

    词性

--- model/word.go
+++ model/word.go
@@ -1,43 +1,57 @@
 package model
 
-type WordType int8
+type WordType int64
 
-//动词1
-//动词2
-//动词3
-//名词
-//i形
-//na形
-//助词
-//副词
-//代词
-//连体
-//接尾
-//连语
-//接辞
-//造语
-//感
-//接
+//	句子		句子
+//	名詞		名词
+//	動詞Ⅰ	动词Ⅰ
+//	動詞Ⅱ	动词Ⅱ
+//	動詞Ⅲ	动词Ⅲ
+//	イ形		い形
+//	ナ形		な形
+//	助詞		助词
+//	代名詞	代词
+//	感
+//	副
+//	副詞		副词
+
+//	连语		前
+//	连体
+//	接		后
+//	接辞
+//	接尾
+//	造语
+//	助数
 
 const (
-	Verb1       = WordType(1)
-	Verb2       = WordType(2)
-	Verb3       = WordType(3)
-	Noun        = WordType(4)
-	AdjectiveI  = WordType(5)
-	AdjectiveNa = WordType(6)
-	Adverb      = WordType(7)
-	Conjunction = WordType(8)
-	Suffix      = WordType(9)
-	Prefix      = WordType(10)
-	Sentence    = WordType(11)
+	Sentence     = WordType(1 << 0)  //	句子
+	Noun         = WordType(1 << 1)  //	名詞		名词
+	Verb1        = WordType(1 << 2)  //	動詞Ⅰ	动词Ⅰ
+	Verb2        = WordType(1 << 3)  //	動詞Ⅱ	动词Ⅱ
+	Verb3        = WordType(1 << 4)  //	動詞Ⅲ	动词Ⅲ
+	AdjectiveI   = WordType(1 << 5)  //	イ形		い形
+	AdjectiveNa  = WordType(1 << 6)  //	ナ形		な形
+	Auxiliary    = WordType(1 << 8)  //	助詞		助词
+	Pronoun      = WordType(1 << 9)  //	代名詞	代词
+	Interjection = WordType(1 << 10) //	感		感叹词
+	Adverb1      = WordType(1 << 11) //	副		副
+	Adverb2      = WordType(1 << 12) //	副詞		副词
+	Conjunction1 = WordType(1 << 13) //			连语
+	Conjunction2 = WordType(1 << 14) //			连体
+	Prefix       = WordType(1 << 15) //			接
+	Prefix       = WordType(1 << 16) //			接词
+	Prefix       = WordType(1 << 17) //			接尾
+	Sentence     = WordType(1 << 18) //			造语
+	Sentence     = WordType(1 << 19) //			助数
 )
 
 type Word struct {
 	Id       int
 	WordType WordType
 	// かな 假名
-	kana string
-	// 漢字
-	kanji *string
+	Kana string
+	// 漢字 汉字
+	Kanji *string
+	//
+
 }
